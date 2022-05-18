61601985f5fb4f5b73b1f50e696705c2a4cc8a0a
MaratBR <marat01q@gmail.com>
2022-05-18 20:26:27 +0700

    bugfix (sequenceState l10n)

--- src/pages/admin/sequence/SequenceView/SequenceStateView.tsx
+++ src/pages/admin/sequence/SequenceView/SequenceStateView.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import S from './SequenceStateView.module.scss';
-import { SequenceState } from 'api/definition';
+import { rest } from 'api/definition';
 import { useTranslation } from 'react-i18next';
 
-function SequenceStateView({ state }: { state: SequenceState }) {
+function SequenceStateView({ state }: { state: rest.SequenceState }) {
   const { t } = useTranslation();
 
-  return <span className={`${S[state]} ${S.state}`}>{t(`sequenceState.${state}`)}</span>;
+  return <span className={`${S[state]} ${S.state}`}>{t(`sequence.state.${state}`)}</span>;
 }
 
 export default React.memo(SequenceStateView);
